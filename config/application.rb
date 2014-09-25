c53742efa475b0677d2b27a248d0acb40f0cdd1f
Ronald Hernandez <2ronald.hernandez@gmail.com>
2014-09-25 13:45:10 -0400

    updated config file for devise, updated the environments files for devise

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+config.assets.initialize_on_precompile = false
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-module SociaLoginRails
+module NeuLynx
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -25,6 +27,6 @@ module SociaLoginRails
     CONFIG.each do |k,v|
       ENV[k.upcase] ||= v
     end
-
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ SociaLoginRails::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  # required for heroku 
+  config.action_mailer.default_url_options = { :host => 'http://neulynx.herokuapp.com/' }
 end
